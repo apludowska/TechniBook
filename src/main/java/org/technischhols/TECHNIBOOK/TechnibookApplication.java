ea80f19d6279ebf33d0b3c0b7c0b906c5b28e2b0
Agnieszka Płudowska <u19_agnplu_lbn@technischools.com>
2024-09-17 14:24:14 +0200

    initlial commit

---
+++ src/main/java/org/technischhols/TECHNIBOOK/TechnibookApplication.java
@@ -0,0 +1,13 @@
+package org.technischhols.TECHNIBOOK;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechnibookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechnibookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/technischhols/TECHNIBOOK/TechnibookApplicationTests.java
@@ -0,0 +1,13 @@
+package org.technischhols.TECHNIBOOK;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechnibookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
